b0c81413ff9b5ef352e66c193c24406b15c24664
Oemer Genc <oemergenc@gmail.com>
2019-04-27 15:18:51 +0200

    added componenttest package

---
+++ src/main/g8/src/test/groovy/$package$/componenttests/AbstractComponentSpec.groovy
@@ -0,0 +1,36 @@
+package $package$.componenttests;
+
+import org.apache.kafka.streams.KafkaStreams
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+abstract class AbstractComponentSpec extends Specification {
+    @Value('${local.server.port}')
+    private int port
+
+    protected PollingConditions pollingConditions = new PollingConditions(timeout: 10)
+
+    @Autowired
+    private KafkaStreams kafkaStreams;
+
+
+    def setupSpec() {
+
+    }
+
+    def setup() {
+
+    }
+
+    def clean() {
+
+    }
+
+    def cleanupSpec() {
+
+    }
+}
